e6996b0abf81a2eeea6bb20bc1bae52df0f345ca
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-03-02 00:39:35 +0530

    Time: 9 ms (20.86%), Space: 7.7 MB (79.51%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,32 +1,22 @@
 class Solution {
+    // here we will try to optimise the space complexity without using any freq map.
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        int freq[nums.size()];
-        //initially mark freq of every element 0
-        for(int i=0;i<nums.size();i++)
-            freq[i]=0;
-        select(nums,ds,ans,freq);
+        addperm(0,nums,ans);
         return ans;
     }
 private:
-    void select(vector<int>&nums,vector<int>& ds,vector<vector<int>>& ans,int freq[]){
-        if(ds.size()==nums.size()){
-            ans.push_back(ds); 
-            // if the size of combination is same as of nums ie it is a permutation
+    void addperm(int ind,vector<int> &nums, vector<vector<int>> &ans){
+        if(ind==nums.size()){
+            ans.push_back(nums);
             return;
         }
-        for(int i=0;i<nums.size();i++){
-            if(!freq[i]){
-                // if freq==0 ie element is no picked.
-                ds.push_back(nums[i]);
-                freq[i]=1; // update its frequency.
-                select(nums,ds,ans,freq);
-                // when backtrack, set freq back to 0 and pop the element.
-                freq[i]=0;
-                ds.pop_back();
-            }
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[ind],nums[i]);
+            addperm(ind+1,nums,ans);
+            swap(nums[ind],nums[i]); 
+            // swap back to return original array to right recursion call while backtrack
         }
     }
 };

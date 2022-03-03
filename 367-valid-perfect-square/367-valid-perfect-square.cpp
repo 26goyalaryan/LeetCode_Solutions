42a337260449bb436095424c98d8fd6d5322eb70
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-03-03 21:34:35 +0530

    Time: 4 ms (23.55%), Space: 5.6 MB (98.76%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        for(long int i=1;i*i<=num;i++){
-            if(i*i==num)
-                return true;
-        }   
+      long int low=0,high=num;
+        long int mid;
+        if(num==1) return true;
+        while(low<=high){
+            mid=(high-low)/2 + low; // find middle element
+            if(mid*mid==num)
+                return true; // if num is square of mid, return true.
+            if(mid*mid<num) // else update low and high using binary search
+                low=mid+1;
+            else
+                high=mid-1;
+        }
         return false;
     }
 };

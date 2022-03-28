407ae31858067e41ac005a38c810ff731253c489
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-03-28 15:28:55 +0530

    Time: 4 ms (89.30%), Space: 14.1 MB (32.08%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -2,32 +2,24 @@ class Solution {
 public:
     bool search(vector<int>& nums, int target) {
         int low=0,high=nums.size()-1;
+        // O(n) 2 pointer approach.  
         while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target)
+            if(nums[low]==target || nums[high]==target)
                 return true;
-            // we have to deal with the case of duplicates where 
-            //a[low]==a[mid]==a[high] , that will cause us problem
-            //so will keep subtracting high and increment low accordingly.
-            if(nums[low]==nums[mid] && nums[high]==nums[mid]){
+            else if(target>nums[low]){
+                while(low<high && nums[low]==nums[low+1])
+                    low++;
                 low++;
+            }
+            // on right half
+            else if(target<nums[high]){
+                while(low<high && nums[high]==nums[high-1])
+                    high--;
                 high--;
             }
-            
-            // if left side is sorted
-            else if(nums[low]<=nums[mid]){
-                //try to find element inn left side
-                if(target>=nums[low] && target<=nums[mid])
-                    high=mid-1;
-                else
-                    low=mid+1;
-            }
-            else{
-                if(target<=nums[high] && target>=nums[mid])
-                    low=mid+1;
-                else
-                    high=mid-1;
-            }
+            // not found
+             else
+                 break;
         }
         return false;
     }

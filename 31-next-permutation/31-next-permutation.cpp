c77d867c8a85b102de2189921f29ceb9837e295f
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-04-03 15:32:20 +0530

    Time: 4 ms (73.50%), Space: 12.2 MB (37.25%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,46 +1,23 @@
-/*
-the all lexographical permutations follow a rule
-it linearly increases from back till a break point. even a single element.
-
-step1- we have to find break point.
-step2-  we again traverse from back and swap with tha first greater with element at
-        break point.
-step3- we know that after break point, the string increases linearly, we just have to             reverse it so as to lower its rank.
-
- eg: 1 3 5 4 2
- step1: we find break point at 5;
- step2: we swap 3 with next greater than 3 ie 4.
- step3: we reverse from 2th index till last.
- so ans become "1 4 2 3 5".
- 
-               ^
-eg:         5   \
-          3   4  \
-        1       2 \
-*/
-
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n=nums.size(),k,l;
-        // step:1 to find breakpoint.
         for(k=n-2;k>=0;k--){
             if(nums[k]<nums[k+1])
                 break;
         }
-        if(k<0)
+        if(k<0){
             reverse(nums.begin(),nums.end());
-        // edge case of 54321 where no break point.
-        // so we directly return the first permutation.
+            return;
+        }
         else{
-            // step:2 to find next greater
-            for(l=n-1;l>k;l--){
-                if(nums[k]<nums[l])
+            for(l=n-1;l>k;l--)
+            {
+                if(nums[l]>nums[k])
                     break;
             }
-            // step:3 swap and reverse.
-            swap(nums[k],nums[l]);
-            reverse(nums.begin()+k+1,nums.end());
         }
+        swap(nums[l],nums[k]);
+        reverse(nums.begin()+k+1,nums.end());
     }
 };

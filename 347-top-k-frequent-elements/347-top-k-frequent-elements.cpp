e9e3d8b590b52cab2e66c0227ea185b2c611ccfd
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-04-11 17:42:53 +0530

    Time: 22 ms (50.95%), Space: 13.8 MB (41.89%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -7,12 +7,13 @@ public:
         for(int it : nums){
             map[it]++;
         } 
-        for(auto it = map.begin(); it != map.end(); it++){
-            pq.push(make_pair(it->second, it->first));
-            if(pq.size() > (int)map.size() - k){
+        for(auto it:map){
+            pq.push({it.second, it.first});
+            if(pq.size()>map.size() - k){
+                // to push only k elements. rather using one more for loop.
                 ans.push_back(pq.top().second);
                 pq.pop();
-            }
+           }
         }
         return ans;
     }

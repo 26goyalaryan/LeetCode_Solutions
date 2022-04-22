eb5861d0820009d39722318b455c8c4bd6baac4b
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-04-22 12:25:48 +0530

    Time: 125 ms (78.19%), Space: 61.2 MB (61.86%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    // we can solve it using flyod cycle algorithm
-    int findDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        if(n>1){
-            int slow=nums[0],fast=nums[nums[0]];
+    int findDuplicate(vector<int>& n) {
+        int m=n.size();
+        if(m>1){
+            int slow=n[0];
+            int fast=n[n[0]];
             while(slow!=fast){
-                slow=nums[slow];
-                fast=nums[nums[fast]];
+                slow=n[slow];
+                fast=n[n[fast]];
             }
-            // the will meet somewhere in loop.
-            // and when fast starts from 0 and slow moves in loop
-            // they will again meet at loops starting point
             fast=0;
             while(slow!=fast){
-                slow=nums[slow];
-                fast=nums[fast];
+                slow=n[slow];
+                fast=n[fast];
             }
             return slow;
         }
-        return -1; // if not found
+        return -1;
     }
 };

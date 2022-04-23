737aa5106a5e2ed988ee5b243dab08ff8f0f8450
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-04-24 00:06:13 +0530

    Time: 8 ms (95.21%), Space: 10.9 MB (44.86%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -6,7 +6,8 @@ public:
         for(int i=0;i<nums.size();i++){
             //cout<<mp[target-nums[i]];
             if(mp.find(target-nums[i])!=mp.end()){
-            //if(mp[target-nums[i]]>0){
+            //if(mp[target-nums[i]]>0)
+            // cant use this bcz we are storing index not freq.{
                 ans.push_back(mp[target-nums[i]]);
                 ans.push_back(i);
                 break;

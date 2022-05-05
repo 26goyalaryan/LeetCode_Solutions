fae4936b6e8c24b9e16c22cd82abc595ccfab783
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-05-06 00:09:50 +0530

    Time: 7 ms (70.74%), Space: 10.7 MB (69.17%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,30 +1,28 @@
 class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
+        sort(arr.begin(),arr.end());
+        vector<int> ds;
+        vector<vector<int>> ans;
+        solve(0,target,ans,arr,ds);
+        return ans;
+    }
 private:
-    // here we will not choose the element but we choose the combination and if its sum = target we 
-    // will push.
-    void fcombination(int ind,int target,vector<int> &arr,vector<int> &ds,vector<vector<int>> &ans){
+    void solve(int ind,int target,vector<vector<int>> &ans,vector<int> &arr,vector<int> &ds){
         if(target==0){
-            ans.push_back(ds); // base case -> if all the elements forms target sum.
+            ans.push_back(ds);
             return;
         }
         for(int i=ind;i<arr.size();i++){
-            
-            // if the element is not picked up in the combination for the first time and 
-            // index is value is similar to previous one
-            
-            if(i>ind && arr[i]==arr[i-1]) continue;
-            if(target<arr[i]) break; // if element to be put in combination is greater than target.
+            if(i>ind && arr[i]==arr[i-1])
+                // i > ind means that its not the first element of sequence 
+                // and producing the duplicates.
+                continue;
+            if(arr[i]>target)
+                break;
             ds.push_back(arr[i]);
-            fcombination(i+1,target-arr[i],arr,ds,ans);
-            ds.pop_back(); // pop back when the left recursion call over
+            solve(i+1,target-arr[i],ans,arr,ds);
+            ds.pop_back();
         }
-    }
-public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> ds;
-        sort(candidates.begin(),candidates.end()); // sort the array so that we can cover duplicates
-        fcombination(0,target,candidates,ds,ans);
-        return ans;
     }
 };

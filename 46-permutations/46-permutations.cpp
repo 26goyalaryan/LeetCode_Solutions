b36fc097bfc23b174bdf4606dfc89d6112feefa9
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-05-06 14:41:50 +0530

    Time: 3 ms (78.09%), Space: 7.7 MB (78.92%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,29 +1,21 @@
 class Solution {
+    // optimise the space complexity ---> no freq map ----> no ds to store permutation
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<int> ds;
         vector<vector<int>> ans;
-        int n=nums.size();
-        int freq[n];
-        for(int i=0;i<n;i++)
-            freq[i]=0;
-        solve(ans,ds,nums,freq);
+        addperm(0,nums,ans);
         return ans;
     }
 private:
-    void solve(vector<vector<int>> &ans,vector<int> &ds,vector<int> &nums,int freq[]){
-        if(ds.size()==nums.size()){
-            ans.push_back(ds);
+    void addperm(int ind,vector<int> &nums, vector<vector<int>> &ans){
+        if(ind==nums.size()){
+            ans.push_back(nums);
             return;
         }
-        for(int i=0;i<nums.size();i++){
-            if(!freq[i]){
-                ds.push_back(nums[i]);
-                freq[i]=1;
-                solve(ans,ds,nums,freq);
-                freq[i]=0;
-                ds.pop_back();
-            }
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[ind],nums[i]);
+            addperm(ind+1,nums,ans);
+            swap(nums[ind],nums[i]); 
         }
     }
 };

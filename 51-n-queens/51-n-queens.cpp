fbab9e1d9e48b665b043aa8fefb53cb8a9c86f4b
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-05-08 15:19:03 +0530

    Time: 8 ms (67.17%), Space: 7.1 MB (95.38%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // driver code
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
         vector<string> board(n);
@@ -7,48 +8,39 @@ public:
         for(int i=0;i<n;i++){
             board[i]=s; // making each row in board empty
         }
-        solve(0,board,ans,n);
+        // hasings declared to check validity.
+        vector<int> 
+            leftRow(n, 0), 
+            upperDiagonal(2 * n - 1, 0), 
+            lowerDiagonal(2 * n - 1, 0);
+        solve(0,board,ans,leftRow,upperDiagonal,lowerDiagonal,n);
         return ans;
     }
-public: void solve(int col,vector<string> &board,vector<vector<string>> &ans,int n){
-    if(col==n){
-        ans.push_back(board); // insert the final chess board in the ans
-        return;
-    }
-    for(int row=0;row<n;row++){
-         if(isSafe(row,col,board,n)){
-            board[row][col]='Q';
-            solve(col+1,board,ans,n); // call for next recursion for next column.
-            board[row][col]='.'; // backtrack and return unaltered board.
+    void solve(int col,vector<string> &board,vector<vector<string>> &ans,vector<int> &leftRow,vector<int> &upperDiagonal,vector<int> &lowerDiagonal,int n){
+        if(col==n){
+            ans.push_back(board); // insert the final chess board in the ans
+            return;
+        }
+        for(int row=0;row<n;row++){
+            
+            //check for validity of the place
+            
+            if(leftRow[row]==0 &&
+              upperDiagonal[row+col]==0 &&
+              lowerDiagonal[n-1+col-row]==0){
+                board[row][col]='Q';
+                leftRow[row]=1;
+                upperDiagonal[row+col]=1;
+                lowerDiagonal[n-1+col-row]=1;
+                
+                solve(col+1,board,ans,leftRow,upperDiagonal,lowerDiagonal,n);
+                
+                // backtrack and unalter the changes
+                board[row][col]='.';
+                leftRow[row]=0;
+                upperDiagonal[row+col]=0;
+                lowerDiagonal[n-1+col-row]=0;
+            }
         }
     }
-}
-
-public: bool isSafe(int row,int col,vector<string> &board,int n){
-    // for upper diagonal
-    int ogrow=row,ogcol=col;
-    while(row>=0 && col>=0){
-        if(board[row][col]=='Q')
-            return false; // ie upper diagonal has Q so 3rd condition will not be valid
-        row--;
-        col--;
-    }
-    col=ogcol;
-    row=ogrow;
-    // check for left collision
-    while(col>=0){
-        if(board[row][col]=='Q')
-            return false;
-        col--;
-    }
-    // for lower left diagonal
-    row=ogrow;
-    col=ogcol;
-    while(row<n && col>=0){
-        if(board[row][col]=='Q')
-            return false;
-        row++;col--;
-    }
-    return true;
-}
 };

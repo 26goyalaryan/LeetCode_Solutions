bbf790d1d5955b18027e767be62bd659b2a39fcc
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-05-08 21:16:07 +0530

    Time: 11 ms (56.11%), Space: 13.7 MB (65.48%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,43 +1,33 @@
 class Solution {
-    /*
-this can be donw using binary search as O(logn) time is required.
-if we found element at mid, then we store its occurance index and for
-first----> we make high=mid-1
-last-----> we make low=mid+1;
-*/
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int low=0,high=nums.size()-1;
-        vector<int> ans;
         int first=-1,last=-1;
-        // for first occurance
+        int low=0,high=nums.size()-1;
         while(low<=high){
-            int mid=low+(high-low)/2;
+            int mid=(high+low)/2;
             if(nums[mid]==target){
                 first=mid;
                 high=mid-1;
-            }
-            else if(nums[mid]>target)
-                high=mid-1;
-            else
+            }else if(target>nums[mid])
                 low=mid+1;
+            else
+                high=mid-1;
         }
-        //for last occurance
         low=0;
         high=nums.size()-1;
         while(low<=high){
-            int mid=low+(high-low)/2;
+            int mid=(high+low)/2;
             if(nums[mid]==target){
                 last=mid;
                 low=mid+1;
-            }
-            else if(nums[mid]>target)
-                high=mid-1;
-            else
+            }else if(target>nums[mid])
                 low=mid+1;
+            else
+                high=mid-1;
         }
-        ans.push_back(first);
-        ans.push_back(last);
+        vector<int> ans(2,-1);
+        ans[0]=first;
+        ans[1]=last;
         return ans;
     }
 };

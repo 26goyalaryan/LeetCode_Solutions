24c74e87143da118bc05746934a4cd126cd95e8c
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-05-08 16:03:27 +0530

    Time: 12 ms (9.36%), Space: 11.1 MB (75.99%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        // we will check whether the  left half is sorted or right half is sorted
+        
+        // the half which is sorted, we will try to find element in that 
+        // ie the element must lie b/w both corner indices of the half
+        
         int low=0,high=nums.size()-1;
         while(low<=high){
-            int mid=low+(high-low)/2;
+            int mid=(high+low)/2;
             if(nums[mid]==target)
                 return mid;
-            // if left side is sorted
+            // if left half is sorted
             if(nums[low]<=nums[mid]){
-                //try to find element inn left side
-                if(target>=nums[low] && target<=nums[mid])
+                if(nums[mid]>=target && nums[low]<=target){
                     high=mid-1;
-                else
+                }else
                     low=mid+1;
-            }
-            else{
-                if(target<=nums[high] && target>=nums[mid])
+            }else{
+                if(nums[mid]<=target && nums[high]>=target)
                     low=mid+1;
                 else
                     high=mid-1;

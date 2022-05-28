94f7ba3a74de9c3ce6d3e1bdac7948359a50d86f
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-05-28 16:56:17 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (15.99%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -2,10 +2,22 @@ class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
        int counter = 0;
+        /*
+        
 	    for(int i=0;i<nums.size()-1;++i)
 	        for(int j=i+1;j<nums.size();++j)
 		        if(nums[i]==nums[j]) 
                     counter++;
-	    return counter;
+        */
+        int temp[101]={0};
+        for(auto it:nums){
+            temp[it]++;
+        }
+        // count the frequency of the no. present
+        for(auto it:temp){
+        // the no. of pairs we can make using n items it (n*(n-1))/2;
+            counter+=(it*(it-1))/2;
+        }
+        return counter;
     }
 };

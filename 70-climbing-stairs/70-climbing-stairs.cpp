98bfd7b09349231034643d33e146073f786e031a
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-06-09 11:26:38 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (30.44%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -3,14 +3,15 @@ public:
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
         if(n<=1)
-            return 1;
-        
-        dp[0]=1;
-        dp[1]=1;
+            return 1; 
+        int left_call=1;
+        int right_call=1;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int curi=left_call+right_call;
+            left_call=right_call;
+            right_call=curi;
         }
-        return dp[n];
+        return right_call;
     }
 };
 /*

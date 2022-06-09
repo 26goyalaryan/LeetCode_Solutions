48ed446b0ad1723db429ed80efb033eaa8fa17a9
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-06-09 10:26:02 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (41.60%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     int fib(int n) {
+        int prev2=0,prev1=1;
+        if(n<=1)
+            return n;
+        for(int i=2;i<=n;i++){
+            int curri=prev2+prev1;
+            prev2=prev1;
+            prev1=curri;
+        }
         /*
         int dp[n+1];
         memset(dp,-1,sizeof dp);
         */
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
+        //vector<int> dp(n+1,-1);
+        return prev1;
     }
+/*
 private:
     int solve(int n,vector<int> &dp){
         if(n<=1)
@@ -17,4 +26,5 @@ private:
         dp[n]=solve(n-1,dp)+solve(n-2,dp);
         return dp[n];
     }
+*/
 };

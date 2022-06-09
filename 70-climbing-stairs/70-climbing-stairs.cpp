41c717605da19533ef52bc6ebd92e2c0d1a2b829
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-06-09 11:23:49 +0530

    Time: 5 ms (9.55%), Space: 6.3 MB (15.34%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -2,8 +2,18 @@ class Solution {
 public:
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-        return solve(n,dp);
+        if(n<=1)
+            return 1;
+        
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
+};
+/*
 private:
     int solve(int n,vector<int> &dp){
         // if standing at 0 -- 1 way to reach ie stand there only.
@@ -17,4 +27,4 @@ private:
         // sum = when you take 1 step + when to take 2 step
         return dp[n]=solve(n-1,dp)+solve(n-2,dp);
     }
-};
+*/

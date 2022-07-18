487ed100bc195fdc44af95c737e2e5fe708e8c38
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-07-18 16:29:22 +0530

    Time: 167 ms (67.75%), Space: 29 MB (35.27%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        int count=0;
-        vector<int> sf(26,0);
-        vector<int> tf(26,0);
-        for(auto it:s)
-            sf[it-'a']++; // count the freq of characters in s.
-        for(auto it:t)
-            tf[it-'a']++; // count the freq of characters in s.
-        for(int i=0;i<26;i++){
-            count+=abs(sf[i]-tf[i]); 
-            
-            /* minimum count will be the total difference of frequency present in either of string
-            eg in "arjun" & "aryan" or the same elements will cancel out each other and rest all will
-            be returned.
-            */
+        int ans=0;
+        vector<int> sf(26,0),tf(26,0);
+        for(auto it:s){
+            sf[it-'a']++;
         }
-        return count;
+        for(auto it:t)
+            tf[it-'a']++;
+        for(int i=0;i<26;i++){
+            if(sf[i]!=tf[i])
+                ans+=abs(sf[i]-tf[i]);
+        }
+        return ans;
     }
 };

931431777f9cf2b5da292925845e5189b338a9bf
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-07-22 00:44:01 +0530

    Time: 49 ms (96.33%), Space: 19.7 MB (75.84%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,38 +1,26 @@
 class Solution {
-    /*
-        if element from start and end we will keep traversing
-        else
-            
-        case1: the string is palindrome from i+1--->j ie ith character is to be removed.
-        case2: the palindrome lies b/w i-->j-1 ie jth character isnt valid
-        
-        else if both the cases return false ie there are more than one pair for the same
-            we will return false;
-            
-    */
 public:
     bool validPalindrome(string s) {
         int i=0,j=s.size()-1;
         int count=0;
         while(i<=j){
-           if(s[i]==s[j]){
-               i++;
-               j--;
-           }else{
-               return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);
-           }
-        }
-        return true;
-    }
-    bool isPalindrome(string &s,int l,int r){
-        int i=l,j=r;
-        while(i<j){
             if(s[i]==s[j]){
                 i++;
                 j--;
+            }else{
+                return ispalindrome(s,i+1,j) || ispalindrome(s,i,j-1);
             }
-            else
+        }
+        return true;
+    }
+private:
+    bool ispalindrome(string &s,int l,int r){
+        int i=l,j=r;
+        while(i<j){
+            if(s[i]!=s[j])
                 return false;
+            i++;
+            j--;
         }
         return true;
     }

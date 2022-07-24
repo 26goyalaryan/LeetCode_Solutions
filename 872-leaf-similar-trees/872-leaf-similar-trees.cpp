1edef17052b1df2650e5fdb84b9d07455ab07531
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-07-24 23:55:15 +0530

    Time: 7 ms (51.46%), Space: 12.8 MB (60.62%) - LeetHub

--- 872-leaf-similar-trees/872-leaf-similar-trees.cpp
+++ 872-leaf-similar-trees/872-leaf-similar-trees.cpp
@@ -12,18 +12,17 @@
 class Solution {
 public:
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        string r1,r2;
-        helper(root1,r1);
-        helper(root2,r2);
-        return r1==r2;
+        string t1="",t2="";
+        helper(root1,t1);
+        helper(root2,t2);
+        return t1==t2?true:false;
     }
-private:
-    void helper(TreeNode* root,string &r){
+    void helper(TreeNode* root,string &t){
         if(root==NULL)
             return;
-        if(!root->right && !root->left)
-            r+=to_string(root->val)+'*';
-        helper(root->left,r);
-        helper(root->right,r);
+        if(root->left==NULL && root->right==NULL)
+            t+=to_string(root->val)+"*";
+        helper(root->left,t);
+        helper(root->right,t);
     }
 };

0c486f0a5cda7ac29df01219d125b5885a039c24
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-08-02 23:09:00 +0530

    Time: 11 ms (92.61%), Space: 21.6 MB (88.87%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -9,28 +9,22 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-// for every node, we will check if the nodes val lies in valid range. 
-// for left node (-infinity,root->val)
-// for right node (root->val,+infinity)
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        return check_BST(root,LONG_MIN,LONG_MAX);
-    }
-private:
-    bool check_BST(TreeNode* root,long min,long max){
         if(root==NULL)
             return true;
-        if(root->val>min && root->val<max)
-        {
-            // for left subtree, the min will remain same and max will be root's val
-            bool left = check_BST(root->left,min,root->val);
-            // for right subtree, the max will remain same and min will be root's val
-            bool right = check_BST(root->right,root->val,max);
+        return helper(root,LONG_MIN,LONG_MAX);
+    }
+    bool helper(TreeNode* root,long min,long max){
+        if(root==NULL)
+            return true;
+        if(root->val > min && root->val < max){
+            bool left=helper(root->left,min,root->val);
+            bool right=helper(root->right,root->val,max);
+            
             return left && right;
-        }
-        else
+        }else
             return false;
     }
 };

fb9a20e186134f7700422dbf2e209f3be7ff64d8
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-08-07 00:19:17 +0530

    Time: 3 ms (74.27%), Space: 6.9 MB (99.48%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
-        //int n=nums.size();
         vector<int> ds;
-        solve(nums,0,ds,ans);
+        helper(0,nums,ans,ds);
         return ans;
     }
-private:
-    void solve(vector<int>& nums,int ind,vector<int> &ds,vector<vector<int>> &ans){
+    
+    void helper(int ind,vector<int>& nums,vector<vector<int>> &ans,vector<int> &ds){
         if(ind==nums.size()){
             ans.push_back(ds);
             return;
         }
         ds.push_back(nums[ind]);
-        solve(nums,ind+1,ds,ans);
+        helper(ind+1,nums,ans,ds);
         ds.pop_back();
-        solve(nums,ind+1,ds,ans);
+        helper(ind+1,nums,ans,ds);
     }
 };

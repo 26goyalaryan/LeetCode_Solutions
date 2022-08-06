f02a0283865d0b6bfff222ca26b3ccb1cf156cdd
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-08-07 00:35:13 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (72.64%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -4,18 +4,20 @@ public:
         sort(nums.begin(),nums.end());
         vector<int> ds;
         vector<vector<int>> ans;
-        solve(0,nums,ds,ans);
+        helper(nums,ds,ans,0);
         return ans;
     }
-private:
-    void solve(int ind,vector<int> &nums,vector<int> &ds,vector<vector<int>> &ans){
-        ans.push_back(ds);
+    
+    void helper(vector<int>& nums,vector<int>& ds,vector<vector<int>> &ans,int ind){
+            ans.push_back(ds);
+            //return;
         for(int i=ind;i<nums.size();i++){
-            if(i!=ind && nums[i]==nums[i-1])
+            if(ind != i && nums[i]==nums[i-1])
                 continue;
             ds.push_back(nums[i]);
-            solve(i+1,nums,ds,ans);
+            helper(nums,ds,ans,i+1);
             ds.pop_back();
+            //helper(nums,ds,ans,i+1);
         }
     }
 };

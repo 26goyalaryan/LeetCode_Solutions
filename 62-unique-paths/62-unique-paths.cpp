ec7c204d1facbf6bdce996b9a68fbb25ea7831b2
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-08-22 22:15:47 +0530

    Time: 7 ms (6.65%), Space: 6.5 MB (46.14%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        // we will count ways to reach (0,0) by either moving left or up
-        // for any cell cells[i,j], total ways to reach this will be 
-        // cells[i,j]= cells[i-1][j] + cells[i][j-1];
-        
-        vector<vector<int>> dp(m,vector<int> (n,0));
+        vector<vector<int>> dp(m,vector<int>(n,-1));
         return helper(m-1,n-1,dp);
     }
     int helper(int m,int n,vector<vector<int>> &dp){
@@ -13,9 +9,9 @@ public:
             return dp[m][n]=0;
         else if(m==0 || n==0)
             return dp[m][n]=1;
-        else if(dp[m][n]!=0)
-                return dp[m][n];
+        else if(dp[m][n]!=-1)
+            return dp[m][n];
         else
-            return dp[m][n]=helper(m-1,n,dp)+helper(m,n-1,dp);
+            return dp[m][n] = helper(m-1,n,dp) + helper(m,n-1,dp);
     }
 };

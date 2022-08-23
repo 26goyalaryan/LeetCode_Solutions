49e8a1fc724ef0853bb96b0c4855cd7293fb7782
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-08-23 15:53:13 +0530

    Time: 18 ms (38.13%), Space: 10.2 MB (30.54%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -6,28 +6,27 @@ public:
         
         int n=grid[0].size();
         
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        vector<vector<int>> dp(m,vector<int>(n));
         
-        return helper(m-1,n-1,grid,dp);
-    }
-    
-    int helper(int m,int n,vector<vector<int>>& grid,vector<vector<int>>& dp){
-        
-        if(m==0 && n==0)
-            return grid[m][n];
-        
-        if(m<0 || n<0)
-            return 0;
-        
-        if(m==0)
-            return dp[m][n] = grid[m][n] + helper(m,n-1,grid,dp);
-        
-        if(n==0)
-            return dp[m][n] = grid[m][n] + helper(m-1,n,grid,dp);
-        
-        if(dp[m][n]!=-1)
-            return dp[m][n];
-        
-        return dp[m][n] = grid[m][n] + min(helper(m-1,n,grid,dp),helper(m,n-1,grid,dp));
+        for(int i=0;i<m;i++){
+            
+            for(int j=0;j<n;j++){
+                
+                if(i==0 && j==0)
+                    
+                    dp[i][j] = grid[i][j];
+                
+                else if(i==0 && j!=0)
+                    dp[i][j] = grid[i][j] + dp[i][j-1];
+                
+                else if(i!=0 && j==0)
+                    dp[i][j] = grid[i][j] + dp[i-1][j];
+                
+                else
+                    dp[i][j] = grid[i][j] + min(dp[i][j-1],dp[i-1][j]);
+                
+            }
+        }
+        return dp[m-1][n-1];
     }
 };

8145e1cd2d9fc712c89d3cccb4a1f9c1d1a89614
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-08-23 14:29:40 +0530

    Time: 3 ms (84.81%), Space: 7.7 MB (66.01%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -4,8 +4,33 @@ public:
         
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return helper(m-1,n-1,grid,dp);
+        
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        
+        for(int i=0;i<m;i++){
+            
+            for(int j=0;j<n;j++){
+                
+                if(grid[i][j]==1)
+                    dp[i][j]=0;
+                
+                else if(i==0 && j==0)
+                    dp[i][j]=1;
+                
+                else{
+                    int up=0,left=0;
+
+                    if(i>0){
+                        up = dp[i-1][j];
+                    }
+                    if(j>0)
+                        left = dp[i][j-1];
+                    
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
     int helper(int m,int n,vector<vector<int>>& grid,vector<vector<int>>& dp){
         if(m>=0 && n>=0 &&  grid[m][n]==1)

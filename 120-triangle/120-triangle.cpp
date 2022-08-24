14b8f0f42765092d826e4bd6d58da63bfb05980a
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-08-24 23:39:50 +0530

    Time: 7 ms (81.53%), Space: 9 MB (31.66%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -3,9 +3,27 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         
         int n=triangle.size();
-        vector<vector<int>> dp(n,vector<int>(triangle[n-1].size(),-1));
-        return helper(0,0,triangle,dp);
+        vector<vector<int>> dp(n,vector<int>(triangle[n-1].size(),0));
+        
+        // fill the base case in table 
+        for(int j=0;j<n;j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        
+        // go opposite to recursion 
+        // recursion was f(0,0) ---> f(n-1,n-1)
+        // tabulation goes from last to (0,0)
+        // the main recurrance remains same 
+        
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                
+                dp[i][j] = triangle[i][j] + min(dp[i+1][j],dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
     }
+    
     int helper(int i,int j,vector<vector<int>>& triangle,vector<vector<int>> &dp){
         
         if(i==triangle.size()-1)

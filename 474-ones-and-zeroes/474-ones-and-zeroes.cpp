b6883e547b411c51d6557f2aba501b0ad2613e24
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-08-26 01:00:46 +0530

    Time: 510 ms (46.44%), Space: 102.9 MB (14.24%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -5,9 +5,33 @@ public:
     
     int findMaxForm(vector<string>& strs, int m, int n) {
         
-        dp.resize(m+1,vector<vector<int>>(n+1,vector<int>(strs.size(),-1)));
+        vector<vector<vector<int>>> dp(strs.size()+1,vector<vector<int>>(m+1,vector<int>(n+1)));
         
-        return helper(strs,m,n,0);
+        for(int i=1;i<=strs.size();i++){
+            
+            int count_1 = count(strs[i-1].begin(),strs[i-1].end(),'1');
+            
+            int count_0 = strs[i-1].size() - count_1;
+            
+            for(int j=0;j<=m;j++){
+                
+                for(int k=0;k<=n;k++){
+                    
+                    if(j>=count_0 && k>=count_1)
+                    {
+                        int pick = 1 + dp[i-1][j-count_0][k-count_1];
+                        
+                        int nt_pick = dp[i-1][j][k];
+                    
+                        dp[i][j][k] = max(pick,nt_pick);
+                    }
+                    else
+                        dp[i][j][k] = dp[i-1][j][k];
+                }
+            }
+        }
+        
+        return dp[strs.size()][m][n];
         
     }
     

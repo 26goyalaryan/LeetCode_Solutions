36271a008a550a545f938e8450d13927d5e85ad9
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-08-26 13:02:34 +0530

    Time: 7 ms (72.38%), Space: 7.7 MB (97.53%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -5,6 +5,16 @@ public:
         vector<int> ans(n+1,0);
         
         for(int i=0;i<=n;i++){
+            
+            if(i%2==0)
+                ans[i]=ans[i/2];
+            else
+                ans[i]=ans[i-1]+1;
+            
+            /*
+            
+            O(n)x O(logn) --> nlogn
+            
             int count=0;
             int num=i;
             
@@ -15,6 +25,7 @@ public:
                 num=num/2; // decimal to binary, we keep dividing the no.
             }
             ans[i] = count;
+            */
         }
         return ans;
     }

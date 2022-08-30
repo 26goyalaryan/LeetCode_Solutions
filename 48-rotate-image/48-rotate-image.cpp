5a92d5312cdfce70534e29c17c8b5d6522e1f269
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-08-31 00:42:26 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (33.50%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,26 +1,13 @@
 class Solution {
-    /* the intution behind the solution is simple 
-        1. transpose the matrix as we see there is reversal of something.
-        2. reverse every row.
-        
-        1 2 3       1 4 7      7 4 1
-        4 5 6 --->  2 5 8 ---> 8 5 2
-        7 8 9       3 6 9      9 6 3
-        
-    */
-    
-    
 public:
-    void rotate(vector<vector<int>>& m) {
-        int n=m.size();
-        // transposing matrix ie the element at [i][j] <---> [j][i]
-        for(int i=0;i<n;i++){
+    void rotate(vector<vector<int>>& matrix) {
+        for(int i=0;i<matrix.size();i++){
             for(int j=0;j<i;j++){
-                swap(m[i][j],m[j][i]);
+                swap(matrix[i][j],matrix[j][i]);
             }
         }
-        // reversing evey row
-        for(int i=0;i<n;i++)
-            reverse(m[i].begin(),m[i].end());
+        for(int i=0;i<matrix.size();i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
     }
 };

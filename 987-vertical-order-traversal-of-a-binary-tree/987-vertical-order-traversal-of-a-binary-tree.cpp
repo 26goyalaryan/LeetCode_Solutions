cadbc4c2b053e134ee8b8ad8bb303cfaea323f48
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-09-24 16:37:23 +0530

    Time: 8 ms (59.45%), Space: 13.6 MB (36.74%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -14,29 +14,24 @@ public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         
         map<int,map<int,multiset<int>>> nodes;
-        
-        queue<pair<TreeNode*,pair<int,int>>> todo;
+        queue<pair<TreeNode*, pair<int,int>>> todo;
         
         todo.push({root,{0,0}});
+        
         while(!todo.empty()){
             auto p=todo.front();
             todo.pop();
-            
-            TreeNode* node=p.first;
-            int x=p.second.first;
-            int y=p.second.second;
+            TreeNode* node = p.first;
+            int x = p.second.first;
+            int y = p.second.second;
             
             nodes[x][y].insert(node->val);
-            // pushing in vertical=x, level=y and value in multiset
-            // multiset stores in ascending order allowing multiple copies
             
             if(node->left)
                 todo.push({node->left,{x-1,y+1}});
             if(node->right)
                 todo.push({node->right,{x+1,y+1}});
-            
         }
-        
         vector<vector<int>> ans;
         for(auto p:nodes){
             vector<int> level;

5d091d2392e65d160d7519ea2a2454d129011092
26goyalaryan <71383064+26goyalaryan@users.noreply.github.com>
2022-09-27 13:00:48 +0530

    Time: 58 ms (25.60%), Space: 10.7 MB (33.23%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set<int> st;
-        int i=0,j=0,ans=0;
-        while(j<s.size()){
-            if(st.count(s[j])==0){
-                // the charchter isnt repeating.
+        int ans = 0;
+        int i = 0, j = 0;
+        unordered_set<char> st;
+        int size = s.size();
+        while (j < size)
+        {
+            if (!st.count(s[j]))
+            {
                 st.insert(s[j]);
-                ans=max(ans,(int)st.size());
+                ans = max(ans, (int)st.size());
                 j++;
-            }else{
+            }
+            else
+            {
                 st.erase(s[i]);
                 i++;
-                /*
-                    a b c d e f a g h 
-                    0 1 2 3 4 5 6 7 8
-                    i             j
-                    ans=6 
-                    
-                    we will remove from start bcz it is possible to have no repeating chars 
-                    from i+1 ---> j ---> n-1
-                */
             }
         }
         return ans;
